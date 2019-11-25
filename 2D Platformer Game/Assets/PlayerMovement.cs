ecee6df6ae46d060f476c13981384bd0b6e1ba56
DavidPeter32 <pal.davidpeter@gmail.com>
2019-11-25 21:59:21 +0200

    character mov3

--- 2D Platformer Game/Assets/PlayerMovement.cs
+++ 2D Platformer Game/Assets/PlayerMovement.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed ;
+    public float jumpForce;
+    private float moveInput;
+    private bool facingRight = true;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
     }
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (facingRight == true && moveInput < 0)
+        {
+            flip();
+        }
+        else if (facingRight == false && moveInput > 0)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+
+    }
+
 }
